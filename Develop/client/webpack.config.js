55f15136e9be2b9c6760c5a7a125546851b96959
SpencerNelsonUW <spencernelsonn@gmail.com>
2022-11-14 11:52:35 -0800

    added database and install info, adding webpack inf now

--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -18,7 +18,35 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      // Webpack plugin condense html, css, js into bundle files.
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'JATE'
+      }),
+      // Inject service worker
+      new InjectManifest({
+        swSrc: "./src-sw.js",
+        swDest: 'src-sw.js',
+      }),
+      // Create Manifest file
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: 'Just Another Text Editor',
+        short_name: 'JATE',
+        description: 'Just another text editor',
+        background_color: '#225ca3',
+        theme_color: '#225ca3',
+        start_url: '/',
+        publicPath: '/',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          },
+        ],        
+      }),
     ],
 
     module: {
